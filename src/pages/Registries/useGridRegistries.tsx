9ff6628f3ff7a442a185531c5db677b147cd86f8
ibah <109962513+ibah111@users.noreply.github.com>
2024-04-05 22:43:17 +0300

    feat(RegistriesGrid): сделал хук на таблицу

---
+++ src/pages/Registries/useGridRegistries.tsx
@@ -0,0 +1,64 @@
+import {
+  GridCallbackDetails,
+  GridColDef,
+  GridPaginationModel,
+  GridValidRowModel,
+} from "@mui/x-data-grid-premium";
+import React from "react";
+import { RegistryModel } from "../../models/Registry.model";
+import RegistriesColumns from "./RegistriesColumns";
+import ListRegistries from "../../api/Cessions/Registries/Get/ListRegistries";
+
+interface GridResult<T extends GridValidRowModel> {
+  columns: GridColDef<T>[];
+  rows: T[];
+  onPaginationModelChange: (
+    model: GridPaginationModel,
+    details: GridCallbackDetails<any>
+  ) => void;
+  paginationModel: GridPaginationModel;
+  refresh: () => void;
+  loading: boolean;
+}
+
+export default function useGridRegistries(): GridResult<RegistryModel> {
+  const columns = RegistriesColumns();
+  const [loading, setLoading] = React.useState(false);
+  const [rows, setRows] = React.useState<RegistryModel[]>([]);
+  const [paginationModel, onPaginationModelChange] =
+    React.useState<GridPaginationModel>({
+      page: 0,
+      pageSize: 25,
+    });
+  const refresh = React.useCallback(() => {
+    console.log("UseGridRegistries");
+    setLoading(true);
+    return ListRegistries({
+      ...paginationModel,
+    })
+      .then((result) => {
+        if (result) {
+          const res = result as RegistryModel[];
+          setRows(res);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, [paginationModel]);
+  /**
+   * отрисовка компонента
+   */
+  React.useEffect(() => {
+    refresh();
+  }, [refresh]);
+  return {
+    loading,
+    columns,
+    onPaginationModelChange,
+    paginationModel,
+    rows,
+    refresh,
+  };
+}
