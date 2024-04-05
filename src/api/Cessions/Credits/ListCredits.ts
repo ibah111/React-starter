f15dc377b0d305bdc6f98f2468d83ff14c4bdc80
ibah <109962513+ibah111@users.noreply.github.com>
2024-04-05 12:04:54 +0300

    feat: начинаю писать апи

---
+++ src/api/Cessions/Credits/ListCredits.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { baseRequest } from "../../../utils/baseRequest";
+
+interface ListCreditsParameters {
+  assignee: number;
+  assignor: number;
+  cession_number: string;
+  page: number;
+  page_size: number;
+  registry: number;
+  search: string;
+  status: string;
+}
+const url = "/api/credits/";
+export default function ListCredits(params: ListCreditsParameters) {
+  try {
+    const req = axios.get(baseRequest + url, {
+      data: params,
+    });
+    console.log("ListCredits: ", req);
+    return req;
+  } catch (error) {
+    console.log("error", error);
+    return error;
+  }
+}
---
+++ src/api/Cessions/Credits/RetrieveCredits.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { baseRequest } from "../../../utils/baseRequest";
+
+interface RetrieveCreditsParameters {
+  id: number;
+}
+const url = "/api/credits/";
+export default function RetrieveCredits(params: RetrieveCreditsParameters) {
+  const urlId = `${url}${params.id}`;
+  try {
+    const req = axios.get(baseRequest + urlId);
+    console.log("RetrieveCredits: ", req);
+    return req;
+  } catch (error) {
+    console.log("error: ", error);
+    return error;
+  }
+}
