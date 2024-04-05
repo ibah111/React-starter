24e367231f3a2b4a06b59fe87caf7e6afca7b9a3
ibah <109962513+ibah111@users.noreply.github.com>
2024-04-05 23:07:54 +0300

    feat: стиль таблицы

--- src/pages/Registries/Registries.tsx
+++ src/pages/Registries/Registries.tsx
@@ -1,11 +1,18 @@
 import { DataGridPremium } from "@mui/x-data-grid-premium";
 import useGridRegistries from "./useGridRegistries";
+import { Grid } from "@mui/material";
 
 export default function Registries() {
   const props = useGridRegistries();
   return (
-    <>
-      <DataGridPremium {...props} />
-    </>
+    <Grid item container xs direction={"column"}>
+      <DataGridPremium
+        {...props}
+        sx={{
+          height: 400,
+          width: "100%",
+        }}
+      />
+    </Grid>
   );
 }
