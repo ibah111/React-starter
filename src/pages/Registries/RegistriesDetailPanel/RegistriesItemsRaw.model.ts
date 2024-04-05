2c3f85ecd8b53a600b1be24e9600881bff616345
ibah <109962513+ibah111@users.noreply.github.com>
2024-04-06 02:48:06 +0300

    feat: changed model

--- src/pages/Registries/RegistriesDetailPanel/RegistriesItemsRaw.model.ts
+++ src/pages/Registries/RegistriesDetailPanel/RegistriesItemsRaw.model.ts
@@ -1,9 +1,26 @@
 export default class RegistriesItemsRawModel {
   id: number;
-  credit: number;
-  errors: {
-    debtor: string;
-    debtor_personal_data: string;
-    credit: string;
+  errors: any;
+  processed: boolean;
+  credit: {
+    date: string;
+    number: string;
+  };
+  debtor: {
+    dob: string;
+    gender: string;
+    last_name: string;
+    first_name: string;
+    middle_name: string;
+    address_actual_main: string;
+    address_registration_main: string;
+  };
+  debtor_personal_data: {
+    inn: number;
+    passport_issue_date: string;
+    passport_issued: string;
+    passport_number: number;
+    passport_series: number;
+    snils: string;
   };
 }
